82333d8975a1fab96fc8a4a249e94a0e00fc4e09
SilverFlin <toledorusso@outlook.com>
2023-03-14 23:57:57 -0700

    Create MinMaxSum.js

---
+++ 02_HackerRank/MinMaxSum.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * Given five positive integers, find the minimum and maximum values that can be calculated by
+ * summing exactly four of the five integers.
+ * Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+ */
+function miniMaxSum(arr) {
+  // calc min and max sum all the array
+  // Sum all the array and found min and max, then extract
+  let total = 0;
+  let currentMin = Infinity;
+  let currentMax = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+    if (arr[i] > currentMax) currentMax = arr[i];
+    if (arr[i] < currentMin) currentMin = arr[i];
+  }
+  let minimumSum = total - currentMax;
+  let maximumSum = total - currentMin;
+  console.log(`${minimumSum} ${maximumSum}`);
+}
